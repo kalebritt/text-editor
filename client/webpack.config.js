7177c15d7387a44adbacb5b40ab14c52e7cd5a3f
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-11-03 11:50:13 -0600

    completed webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -60,8 +60,15 @@ module.exports = () => {
           test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
-            
-          }
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
         },
       ],
     },
